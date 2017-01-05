ab3226054e7532b4c9d3a84ddbab52eaf4d6ad3c
damon patron <kryptonian03@hotmail.com>
2017-01-05 12:17:52 +1000

    Initial Commit

---
+++ SaveTheKittensSaveTheHairlessOne/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SaveTheKittensSaveTheHairlessOne
+//
+//  Created by damon patron on 5/01/2017.
+//  Copyright © 2017 PriApps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
